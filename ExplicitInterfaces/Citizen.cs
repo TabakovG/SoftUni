9bed01495d1f805621d778430b961fd084abb3dc
TabakovG <gnt137@gmail.com>
2022-07-13 15:25:48 +0300

    Explicit Interface access exercise

---
+++ ExplicitInterfaces/Citizen.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExplicitInterfaces
+{
+    internal class Citizen : IResident, IPerson
+    {
+        private string name;
+        
+
+        public string Name
+        {
+            get { return this.name; }
+            private set { this.name = value; }
+        }
+
+        public int Age { get; private set; }
+
+        public string Country { get; private set; }
+
+        string IResident.GetName()
+        {
+            return "Mr/Ms/Mrs " + this.Name;
+        }
+        string IPerson.GetName()
+        {
+            return this.name;
+        }
+
+        public Citizen(string name, string country, int age)
+        {
+            this.Name = name;
+            this.Country = country;
+            this.Age = age;
+        }
+    }
+}
---
+++ ExplicitInterfaces/StartUp.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExplicitInterfaces
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string cmd = Console.ReadLine();
+            while (cmd != "End")
+            {
+                string[] data = cmd.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var citizen = new Citizen(data[0], data[1], int.Parse(data[2]));
+                IPerson person = citizen;
+                IResident resident = citizen;
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+                cmd = Console.ReadLine();
+            }
+        }
+    }
+}
